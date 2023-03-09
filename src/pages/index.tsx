a4a6e33cea307f7072e01877f840432e56e016b8
MaxParastiuk <parastykm@gmail.com>
2023-03-09 17:46:37 +0200

    create app

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import Layout from "@/components/layout";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+	return (
+		<>
+			<Head>
+				<h1>Cinema</h1>
+			</Head>
+			<Layout>
+				<main className={styles.main}>adsasd</main>
+			</Layout>
+		</>
+	);
+}
