3e48a0980a4f786ca2f9b108cde5273b8d528bac
MaxParastiuk <parastykm@gmail.com>
2023-03-11 14:02:37 +0200

    add pagination

--- src/pages/films/[name].tsx
+++ src/pages/films/[name].tsx
@@ -2,23 +2,33 @@ import FilmItem from "@/components/FilmItem";
 import { GetServerSideProps, InferGetServerSidePropsType } from "next/types";
 import { IListFilm } from "@/interfaces/IListFilm";
 import { API_ALL_FILMS } from "../../constants/index";
+import Pagination from "@/components/paginatation";
 
 interface FilmsProps {
 	data: {
 		Search: IListFilm[];
+		totalResults: string;
 	};
 }
 
 export default function Films({ data }: FilmsProps) {
 	console.log(data);
-	const { Search } = data;
+	const { Search, totalResults } = data;
 	return (
 		<>
-			<ul>
-				{Search.map((el) => (
-					<h1>{el.Title}</h1>
+			<ul className='flex mt-20 my-gap  mx-24 max-w-full px-4 sm:px-6 lg:px-8 flex-wrap gap-y-14 gap-x-4'>
+				{Search.map((el, index) => (
+					<li className='flex flex-col my-flex-basis ' key={index}>
+						<img src={el.Poster} alt='poster' />
+						<div className=''>
+							<p>{el.Title}</p>
+							<p>{el.Type}</p>
+							<p>{el.Year}</p>
+						</div>
+					</li>
 				))}
 			</ul>
+			<Pagination totalResult={totalResults} />
 		</>
 	);
 }
